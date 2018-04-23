cd88600ffc764ee93829ff17ff2fd3ae7581616a
Lucas Herique Ronqui de Souza <lucas.ronqui.souza@gmail.com>
2018-04-22 22:30:24 -0300

    another one

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require('express')
 const app = express()
-var path = require('path');
+const path = require('path');
+const request = require('request');
+const fs = require('fs');
+
+setInterval(function(){
+  let dataAual = new Date()
+  let dia = dataAual.getUTCDate()
+  let mes = dataAual.getUTCMonth() + 1
+  let ano = dataAual.getUTCFullYear()
+  let hora = dataAual.getHours()
+  let minuto = dataAual.getMinutes()
+
+  request({url: 'https://www.animestelecine.net/'},
+      function(error, response, body){
+        fs.writeFile("./assets/db/teste-"+ano+"-"+mes+"-"+dia+"-"+hora+"-"+minuto+".txt",body,function(err){
+          if(err)
+				      console.log(err)
+        })
+        console.log(body)
+        console.log("fim da requisição")
+  			if(error)
+  				console.log(error)
+  		})
+}, 60100)
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
   app.use('/public', express.static('assets'));
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(80, function () {
+  console.log('Example app listening on port 80!')
 })
